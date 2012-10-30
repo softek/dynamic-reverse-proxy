3065497fcee634ef0c4219f9aee27e53b72c5ea7
Andrew Dunkman <andrew@dunkman.org>
2012-10-30 10:08:51 -0500

    Paths are now prefixes (more than one segment is allowed)

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 var DynamicProxy = require("./lib/dynamicProxy");
-
-module.exports = function (server) {
-   if (!server) { throw new Error("HTTP or HTTPS server must be supplied."); }
-   return new DynamicProxy(server);
-};
+module.exports = function () { return new DynamicProxy(); };
