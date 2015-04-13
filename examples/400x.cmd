970ea0c575d9dd476368e1f17bb54eb29b923e81
Jeremy Sellars <jeremy.sellars@softekinc.com>
2015-04-13 17:07:33 -0500

    Example curl commands to register example routes

---
+++ examples/400x.cmd
@@ -0,0 +1,3 @@
+curl -H "Content-Type: application/json" -d @4000.json http://localhost/register
+curl -H "Content-Type: application/json" -d @4001.json http://localhost/register
+curl -H "Content-Type: application/json" -d @4002.json http://localhost/register
